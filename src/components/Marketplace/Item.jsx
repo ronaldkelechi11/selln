fa2410687c3b0a4efbdb0d8e9cd1d1a6e5d2d8c1
Ronald Kelechi <ronaldkelechi11@gmail.com>
2023-09-27 22:40:54 +0100

    Working on the item wrapper and item modal

--- src/components/Marketplace/Item.jsx
+++ src/components/Marketplace/Item.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
-
-const Item = () => {
+import { FaLocationPin } from "react-icons/fa6";
+const Item = ({ item }) => {
     return (
-        <div className=''>
+        <div className='w-full h-[250px] max-h-[300px] rounded-xl shadow-md flex flex-col justify-evenly p-3'>
+            <img src={item.image[0]} className='h-[100px]' alt="" />
+            <div className="flex flex-row justify-center gap-1">
+                <span className='text-primary font-billabong text-xl'>N</span>
+                <div className="text-primary text-center font-billabong text-xl">{item.price}</div>
+            </div>
+            <div className="text-black text-xl font-billabong">{item.name}</div>
+            <div className="text-grey text-[14px] font-billabong">{item.condition}</div>
 
+            <div className="flex flex-row bg-grey rounded-xl p-2 self-start gap-1">
+                <FaLocationPin size={12} />
+                <div className="text-black font-billabong text-[12px]">{item.state}</div>
+            </div>
         </div>
     )
 }
--- src/components/Marketplace/ItemWrapper.jsx
+++ src/components/Marketplace/ItemWrapper.jsx
@@ -1,9 +1,28 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Item from "./Item";
 
 const ItemWrapper = () => {
-    console.log(window.location);
+    const [itemArrayList, setItemArrayList] = useState([])
+
+    useEffect(() => {
+        axios.get("/src/__tests__/ads.json",)
+            .then((result) => {
+                console.log(result.data);
+                setItemArrayList(result.data)
+            }).catch((err) => {
+                console.log(err);
+            });
+    }, [])
+
+
     return (
-        <div className='w-full h-screen bg-white'>
-            { }
+        <div className='w-full h-screen bg-white grid grid-cols-2 md:grid-cols-5 md:p-5 p-3 gap-3'>
+            {
+                itemArrayList.map(item => {
+                    return (<Item key={item.id} item={item} />)
+                })
+            }
         </div>
     )
 }
